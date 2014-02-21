fe8bed706945cf13b58b31d6c0614b43c920c3e0
KUOKA Yusuke <ykuoka@gmail.com>
2014-02-21 19:07:59 +0900

    Add prototypes for all known APIs except Pusher-related ones

--- src/main/scala/com/github/mumoshu/idobata/Idobata.scala
+++ src/main/scala/com/github/mumoshu/idobata/Idobata.scala
@@ -11,20 +11,21 @@ case class Records(bot: Bot)
 case class Bot(id: Int, name: String, iconURL: String, apiToken: String, status: String, channelName: String)
 
 case class Idobata(apiToken: String = "28df745bf9e7ea110de8e02d0d641e44") {
+  type ID = Long
+
   val IdobataURL = "https://idobata.io"
   val IdobataPusherKey = "44ffe67af1c7035be764"
   val APIToken = apiToken
 
-  val apiSeedRequest = url(s"${IdobataURL}/api/seed")
-  val messagesRequest = url(s"${IdobataURL}/api/messages")
-  val roomsRequest = url(s"${IdobataURL}/api/rooms")
-  val usersRequest = url(s"${IdobataURL}/api/users")
-
   val headers = Map("X-API-Token" -> Seq(APIToken), "User-Agent" -> Seq(s"hubot-idobata / v0.0.2"))
-  val apiSeedRequestWithHeaders = apiSeedRequest.setHeaders(headers)
-  
-  def apiSeedAsString: Future[String] = Http(apiSeedRequestWithHeaders OK as.String)
-  def apiSeedAsJson: Future[JValue] = Http(apiSeedRequestWithHeaders OK as.json4s.Json)
+  val apiSeedRequest = url(s"${IdobataURL}/api/seed").setHeaders(headers)
+  val messagesRequest = url(s"${IdobataURL}/api/messages").setHeaders(headers)
+  val roomsRequest = url(s"${IdobataURL}/api/rooms").setHeaders(headers)
+  val usersRequest = url(s"${IdobataURL}/api/users").setHeaders(headers)
+  val botsRequest = url(s"${IdobataURL}/api/bots").setHeaders(headers)
+
+  def apiSeedAsString: Future[String] = Http(apiSeedRequest OK as.String)
+  def apiSeedAsJson: Future[JValue] = Http(apiSeedRequest OK as.json4s.Json)
 
   def apiSeed: Future[APISeed] = {
     implicit val fmts = DefaultFormats
@@ -49,14 +50,40 @@ case class Idobata(apiToken: String = "28df745bf9e7ea110de8e02d0d641e44") {
   }
 
   def sendMessage(roomId: Int, source: String) = {
-    Http(messagesRequest.setHeaders(headers) << Map("message[room_id]" -> roomId.toString, "message[source]" -> source) OK as.String)
+    Http(messagesRequest << Map("message[room_id]" -> roomId.toString, "message[source]" -> source) OK as.String)
   }
 
   def getRooms(organizationSlug: String, roomName: String) = {
-    Http(roomsRequest.setHeaders(headers) <<? Map("organization_slug" -> organizationSlug, "room_name" -> roomName) OK as.String)
+    Http(roomsRequest <<? Map("organization_slug" -> organizationSlug, "room_name" -> roomName) OK as.String)
+  }
+
+  def getRooms(ids: Traversable[ID]) = {
+    Http(roomsRequest <<? Map("ids[]" -> ids.mkString(",")) OK as.String)
+  }
+
+  def getRoom(id: ID) = {
+    Http(roomsRequest / id OK as.String)
   }
 
   def touchRoom(roomId: Int) = {
     Http((roomsRequest / roomId.toString).POST OK as.String)
   }
+
+  def getUsers(ids: Traversable[ID]) =
+    Http(usersRequest <<? Map("ids[]" -> ids.mkString(",")) OK as.String)
+
+  def getUser(id: ID) =
+    Http(usersRequest / id OK as.String)
+
+  def getBots(ids: Traversable[ID]) =
+    Http(botsRequest <<? Map("ids[]" -> ids.mkString(",")) OK as.String)
+
+  def getMessages(roomId: ID, olderThan: ID) =
+    Http(messagesRequest <<? Map("room_id" -> roomId.toString, "older_than" -> olderThan.toString) OK as.String)
+
+  def getMessages(ids: Traversable[ID]) =
+    Http(messagesRequest <<? Map("ids[]" -> ids.mkString(",")) OK as.String)
+
+  def deleteMessage(id: ID) =
+    Http((messagesRequest / id DELETE) OK as.String)
 }
--- src/test/scala/com/github/mumoshu/idobata/IdobataFunctionalSpec.scala
+++ src/test/scala/com/github/mumoshu/idobata/IdobataFunctionalSpec.scala
@@ -4,23 +4,34 @@ import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 import com.typesafe.scalalogging.slf4j.Logging
 import org.scalatest.time.{Millis, Seconds, Span}
+import scala.concurrent.Future
 
 class IdobataFunctionalSpec extends FlatSpec with Matchers with ScalaFutures with Logging {
 
   implicit override val patienceConfig =
     PatienceConfig(timeout =  Span(5, Seconds), interval = Span(1, Millis))
 
+  private val roomId = 1518
+  private val userId = 1877
+  private val botId = 1880
+
   "A Idobata" should "send messages to rooms" in {
 
-
     val idobata = Idobata()
-    whenReady(idobata.getRooms("mumoshu", "testroom")) { rooms =>
-      logger.info(rooms)
-      rooms shouldNot be ("")
+
+    def test[A](future: Idobata => Future[A]) = whenReady(future(idobata)){ response =>
+      logger.info(response.toString)
     }
-    whenReady(idobata.sendMessage(1518, "Hi!")) { response =>
-      logger.info(response)
-    }
+
+    test(_.getRooms("mumoshu", "testroom"))
+    test(_.getRoom(roomId))
+    test(_.getRooms(Seq(roomId)))
+    test(_.sendMessage(roomId, "Hi!"))
+    test(_.getMessages(roomId, olderThan = 1513410))
+    test(_.getMessages(Seq(1513410)))
+    test(_.getUser(userId))
+    test(_.getUsers(Seq(userId)))
+    test(_.getBots(Seq(botId)))
   }
 
 }
